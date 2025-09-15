bf6f1c74bda2b8fa6fa7efa05da57c4c7649e684
Christophe Monard <cmonard@gaya.fr>
2025-09-15 15:04:16 +0200

    [TASK] enable usage of fractor.project.php custom file

--- res/fractor.php
+++ res/fractor.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 use a9f\Fractor\Configuration\FractorConfiguration;
 use a9f\Typo3Fractor\Set\Typo3LevelSetList;
 
-return FractorConfiguration::configure()
+$fractorConfigBuilder = FractorConfiguration::configure()
     ->withPaths([
         __DIR__ . '/packages'
     ])
     ->withSets([
         Typo3LevelSetList::UP_TO_TYPO3_13,
     ]);
+
+if (file_exists('fractor.project.php')) {
+    include_once 'fractor.project.php';
+}
+
+return $fractorConfigBuilder;
